cbb4ff628764f6f626b5ee753cebd8b3e60d454d
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-06-22 12:35:19 +0800

    *figured out how to use postman +set up working switch case

--- src/api/index.js
+++ src/api/index.js
@@ -5,10 +5,20 @@ import facets from './facets';
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
-
-	// perhaps expose some API metadata at the root
+	api.post('/post', (req, res) => {
+		const action = req.body.queryResult.action;
+		const output = {"Speech" : " ", "displayText" : " "}
+		switch(action){
+			case 'inputWelcome':
+				output["Speech"] = "Welcome to book rental! What would you like me to call you?"
+				output["displayText"] = "Welcome to book rental! What would you like me to call you?"
+				return res.json(output);
+				break;
+			}
+	})
+	api.get('/getName',function (req,res){
+		res.send('Swarup Bam');
+	});
 	api.get('/', (req, res) => {
 		res.send('fite me')
 	});
